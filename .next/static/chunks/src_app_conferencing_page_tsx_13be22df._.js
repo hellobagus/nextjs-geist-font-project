667999437dc72a49c6e650bb91753663e70f2840
HelloBagus <bagustrinanda@yahoo.com>
2025-05-16 21:39:09 +0700

    Add .next/static/chunks/src_app_conferencing_page_tsx_13be22df._.js 1747406348542

---
+++ .next/static/chunks/src_app_conferencing_page_tsx_13be22df._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_dd04b166._.js",
+  "static/chunks/src_01ba8acd._.js"
+],
+    source: "dynamic"
+});
