317e74234c85e1e6d2abd59ebe18dfb31fa07f17
HelloBagus <bagustrinanda@yahoo.com>
2025-05-16 21:38:41 +0700

    Add .next/static/chunks/src_app_language_page_tsx_13be22df._.js 1747406320820

---
+++ .next/static/chunks/src_app_language_page_tsx_13be22df._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_70445223._.js",
+  "static/chunks/src_828c8736._.js"
+],
+    source: "dynamic"
+});
