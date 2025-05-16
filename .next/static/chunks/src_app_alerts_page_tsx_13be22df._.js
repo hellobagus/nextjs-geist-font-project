9b9210df666e75c67eded510c7b24b5d499043cc
HelloBagus <bagustrinanda@yahoo.com>
2025-05-16 21:38:19 +0700

    Add .next/static/chunks/src_app_alerts_page_tsx_13be22df._.js 1747406298896

---
+++ .next/static/chunks/src_app_alerts_page_tsx_13be22df._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_7ed65ff8._.js",
+  "static/chunks/src_65d39c10._.js"
+],
+    source: "dynamic"
+});
